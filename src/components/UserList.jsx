1bb9ab62bc1a61bdbc0a0c84f1431fcb0e243a3a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-11-11 16:29:05 +0200

    Move users to separate file.

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
-
-const USERS = [
-    { id: 1001, name: "Bob" },
-    { id: 1005, name: "Oleg" },
-    { id: 1090, name: "Yura" }
-];
+import { USERS } from "./users-mock";
 
 function UserList() {
 
---
+++ src/components/users-mock.js
@@ -0,0 +1,5 @@
+export const USERS = [
+    { id: 1001, name: "Bob" },
+    { id: 1005, name: "Oleg" },
+    { id: 1090, name: "Yura" }
+];
